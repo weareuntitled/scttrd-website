4b03e8c4632f16861c1b894574be9cfcf104abbf
Daniel Peters <daniel.peters@elephant-agency.de>
2026-08-21 16:09:58 +0200

    feat: simple password-only cms, github token via api/auth, no oauth

--- api/auth.js
+++ api/auth.js
@@ -1,24 +1,7 @@
 export default async function handler(req, res) {
-  const clientId = process.env.GITHUB_OAUTH_CLIENT_ID;
-  const clientSecret = process.env.GITHUB_OAUTH_CLIENT_SECRET;
-  if (!clientId || !clientSecret) {
-    return res.status(503).send('OAuth not configured');
-  }
-  const url = new URL(req.url, `https://${req.headers.host}`);
-  const code = url.searchParams.get('code');
-  if (!code) {
-    const redirect = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo&redirect_uri=${encodeURIComponent(`https://${req.headers.host}/api/auth`)}`;
-    return res.redirect(redirect);
-  }
-  const tokenRes = await fetch('https://github.com/login/oauth/access_token', {
-    method: 'POST',
-    headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
-    body: JSON.stringify({ client_id: clientId, client_secret: clientSecret, code }),
-  });
-  const data = await tokenRes.json();
-  const token = data.access_token;
-  if (!token) return res.status(401).send('No token');
-  const html = `<!doctype html><html><body><script>window.opener.postMessage('authorization:github:success:${JSON.stringify({ token })}', '*'); window.close();</script></body></html>`;
+  const token = process.env.GITHUB_TOKEN;
+  if (!token) return res.status(503).send('GITHUB_TOKEN missing');
+  const html = `<!doctype html><html><body><script>var t=${JSON.stringify(token)};window.opener.postMessage('authorization:github:success:'+JSON.stringify({token:t}),'*');window.close();</script></body></html>`;
   res.setHeader('Content-Type', 'text/html');
   return res.send(html);
 }
